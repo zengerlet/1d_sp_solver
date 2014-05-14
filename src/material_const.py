f39b5fc20fa694240ecd2c0a2934ab16a353a4c8
Thomas Zengerle <zengerlet@student.ethz.ch>
2014-05-14 22:45:14 +0200

    various material parameters added

--- src/material_const.py
+++ src/material_const.py
@@ -1,7 +1,33 @@
 
 #material dictionaries
-GaAsnn = {'meff': 0.067, 'eps': 12.93, 'cb_e': 2.97900}				# nextnano
-Al33Ga67Asnn = {'meff': 0.09439, 'eps': 11.9842, 'cb_e': 3.26466}		# nextnano
-Al20Ga80Asnn = {'meff': 0.0836, 'eps': 12.3568, 'cb_e': 3.14420}		# nextnano
-Al24Ga76Asnn = {'meff': 0.08692, 'eps': 12.2422, 'cb_e': 3.18016}		# nextnano
-AlAsnn = {'meff': 0.15, 'eps': 10.064, 'cb_e': 4.049}				# nextnano
+
+
+#nextnano
+GaAsnn =            {'meff': 0.067, 'eps': 12.93, 'cb_e': 2.97900}				
+Al33Ga67Asnn =      {'meff': 0.09439, 'eps': 11.9842, 'cb_e': 3.26466}	
+Al20Ga80Asnn =      {'meff': 0.0836, 'eps': 12.3568, 'cb_e': 3.14420}
+Al24Ga76Asnn =      {'meff': 0.08692, 'eps': 12.2422, 'cb_e': 3.18016}		
+AlAsnn =            {'meff': 0.15, 'eps': 10.064, 'cb_e': 4.049}				
+
+
+#ioffe
+'''
+AlxGa(1-x)As
+#http://www.ioffe.ru/SVA/NSM/Semicond/AlGaAs/bandstr.html
+Conduction band discontinuity:
+    x<0.41       ΔEc = 0.79x (eV)
+    x>0.41       ΔEc = 0.475-0.335x+0.143x2 (eV) 
+
+@300K
+http://www.ioffe.ru/SVA/NSM/Semicond/AlGaAs/basic.html
+Dielectric constant (static)    12.90-2.84x
+Effective electron mass me  0.063+0.083x mo (x<0.45)
+'''
+
+GaAsff =            {'meff': 0.063000, 'eps': 12.900, 'cb_e': 0.00000}				
+Al1708Ga8292ff =    {'meff': 0.077176, 'eps': 12.415, 'cb_e': 0.13493}				
+Al1952Ga8048ff =    {'meff': 0.079202, 'eps': 12.346, 'cb_e': 0.15421}				
+Al20Ga80ff =        {'meff': 0.079600, 'eps': 12.332, 'cb_e': 0.15800}				
+Al2379Ga7621ff =    {'meff': 0.082746, 'eps': 12.224, 'cb_e': 0.18794}				
+Al24a76ff =         {'meff': 0.082920, 'eps': 12.218, 'cb_e': 0.18960}				
+Al244Ga756ff =      {'meff': 0.083252, 'eps': 12.207, 'cb_e': 0.19276}				
